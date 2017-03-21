69700e7d30a590b751b7e4799121e329e46461b5
Andreas Nyhrén <andreas.nyhren@gmail.com>
2017-03-21 09:59:56 +0100

    service worker

--- sw.js
+++ sw.js
@@ -1,14 +1,27 @@
-self.addEventListener('install', function(e) {
- e.waitUntil(
-   caches.open('nyhren').then(function(cache) {
-     return cache.addAll([
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('nyhren').then(cache => {
+      return cache.addAll([
        '/',
        '/index.html',
        '/index.html?homescreen=1',
        '/?homescreen=1',
        'https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css',
-       'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css',
-     ]);
-   })
- );
+       'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'
+      ])
+      .then(() => self.skipWaiting());
+    })
+  )
+});
+
+self.addEventListener('activate',  event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
