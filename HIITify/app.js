a38ebff171dc8452e74b68c5021bb5beac0e0536
Andreas Nyhren <andreas.nyhren@gmail.com>
2017-07-26 10:57:15 +0200

    cont replacing jquery

---
+++ HIITify/app.js
@@ -0,0 +1,201 @@
+"use strict"
+var App = {
+	client_id: '9d4ecfc733cf415887763c509a274bc5',
+	init: function() {
+		console.log('app');
+		if (!Spotify.token &&!location.hash) {
+			Spotify.client_id = App.client_id;
+			Spotify.login();
+		} else {
+			var o = new URLSearchParams(location.hash.substring(1));
+			//TODO: check state matches
+			Spotify.token = o.get('access_token');
+			console.log(Spotify.token);
+			Spotify.devices().then((data) => {console.log(data);});
+			Spotify.me().then((data) => {				
+				console.log(data);
+			});		
+			$('#playlists').empty();	
+			getPlayLists(0, 50);
+		}
+	}
+};
+
+/* UI
+/
+about + connect
+/home
+select device + rescan
+hiitfy ui
+	
+*/
+
+var totTime = 1*60;
+var loTime = 15;
+var hiTime = 10;
+var isHigh = false;
+var songRemaining = -1;
+var totTimer = new Timer({
+	tick: 250,
+	onend: function() {
+		loTimer.stop();
+		hiTimer.stop();
+		Spotify.pauseSong();
+		console.log('tot end');
+	},
+	ontick: function() {
+		//console.log('ontick');
+		$("#tot").text(Math.floor(totTimer.getDuration()/1000).toMMSS());
+		$("#curr").text(Math.floor(isHigh?hiTimer.getDuration()/1000:loTimer.getDuration()/1000).toSS());
+		// TODO: check curr interval if interval < track.duration_ms - startAt -> restart track
+	    //60-45 - left > 0
+	    songRemaining = (isHigh?hiTime:loTime)-(playlistTracks[currSong].duration_ms/1000)+parseFloat(analysis[playlistTracks[currSong].id])-(isHigh?hiTimer.getDuration()/1000:loTimer.getDuration()/1000);
+		//console.log((isHigh?hiTime:loTime),(playlistTracks[currSong].duration_ms/1000),parseFloat(analysis[playlistTracks[currSong].id]),(isHigh?hiTimer.getDuration()/1000:loTimer.getDuration()/1000));
+		//console.log((isHigh?hiTime:loTime)-(playlistTracks[currSong].duration_ms/1000)+parseFloat(analysis[playlistTracks[currSong].id])-(isHigh?hiTimer.getDuration()/1000:loTimer.getDuration()/1000));
+		
+		if (songRemaining > 0 && (Math.ceil(songRemaining + 0.750)==1)) {
+			console.log("RESTART");
+			console.log((isHigh?hiTime:loTime)-(playlistTracks[currSong].duration_ms/1000)+parseFloat(analysis[playlistTracks[currSong].id])-(isHigh?hiTimer.getDuration()/1000:loTimer.getDuration()/1000));
+			if (Spotify.isPlaying)
+				handlePlay();
+		} 		
+	}
+});
+var loTimer = new Timer({
+	onstart: function() {console.log('lo start');handlePlay();},
+	onend: function() {isHigh=true;++currSong;hiTimer.start(hiTime); console.log('lo end');}
+});
+var hiTimer = new Timer({
+	onstart: function() {console.log('hi start');handlePlay();},
+	onend: function() {isHigh=false;++currSong;loTimer.start(loTime);console.log('hi end');}
+});
+
+var outgoing = 0;
+var playlistTracks = [];
+var analysis = [];
+var currSong = 0;
+
+function fixTotTime() {
+	var cycle = loTime + hiTime;
+	return Math.ceil(totTime / cycle)*cycle; 
+}
+
+function start() {
+	currSong = 0;
+	totTimer.start(fixTotTime(totTime));
+	loTimer.start(loTime);
+}
+
+function handlePlay() {
+	Spotify.isPlaying = false;
+	console.log(currSong, playlistTracks[currSong], analysis[playlistTracks[currSong].id]);
+	if (currSong < playlistTracks.length) {
+		Spotify.startSong(playlistTracks[currSong].id, analysis[playlistTracks[currSong].id]);
+		$("#artist").text(playlistTracks[currSong].artists[0].name);
+		$("#title").text(playlistTracks[currSong].name);
+		$("#artwork").attr("src", playlistTracks[currSong].album.images[0].url);
+	}
+}
+
+function getPlayLists(offset, limit) {	
+	Spotify.getPlayLists(offset, limit).then((data) => {
+		console.log(data);
+		data.items.forEach((pl) => {
+			$('#playlists')					
+				.append($('<option>', { value : pl.href })	
+				.text(pl.name));
+		});
+		if (data.next) {
+			getPlayLists(data.offset + data.limit, data.limit);
+		}
+	});
+}
+
+function analyze() {
+	// reset any previous analysis (and current playlist tracks)
+	playlistTracks = [];
+	analysis = [];
+	getPlaylistTracks(0,100,$('#playlists').val(),null, function(data, next) {
+		var ids = "";
+		outgoing += data.items.length;
+		for (var i = 0; i < data.items.length; i++) {
+			if (data.items[i].track.is_playable) {
+				if (i > 0) ids += ",";
+				ids += data.items[i].track.id;
+				//console.log(data.items[i]);
+				playlistTracks.push(data.items[i].track);
+			} else {
+				console.log('track not available', data.items[i].track);
+			}
+		}
+		getAudioFeatures(ids);
+		if (!next) {
+			console.log('playlist tracks done ' + outgoing);
+		}
+	});
+}
+
+function getPlaylistTracks(offset,limit,playlist,href, cb) {
+	Spotify.getPlaylistTracks(offset,limit,playlist,href).then((data) => {
+		if (data && data.next) {
+			getPlaylistTracks(data.offset + data.limit, data.limit, null, data.next,cb);
+		} 
+		cb(data, data.next);
+	});
+}
+
+function getAudioFeatures(ids) {
+	Spotify.getAudioFeatures(ids).then((data) => {
+		//console.log(data);
+		for (var i = 0; i < data.audio_features.length; i++) {
+			//console.log(data.audio_features[i].id + " "+ data.audio_features[i].loudness);
+			var lsid = localStorage.getItem(data.audio_features[i].id);
+			if (!lsid) {
+				Spotify.getAudioAnalysis(data.audio_features[i].analysis_url).then(function(response) {
+					Promise.all([Promise.resolve(response.url), response.json()]).then(function(o){
+						var url = o[0];
+						var data = o[1];
+						var loudness = data.track.loudness;
+						var eofi = data.track.end_of_fade_in;
+						var startAt = 0;
+						for (var i = 0; i < data.sections.length;i++) {
+							if (data.sections[i].duration >= 20 && data.sections[i].loudness > loudness) {
+								startAt = data.sections[i].start;
+								break;
+							}
+						}
+						startAt = Math.max(startAt, eofi);
+						var str = "/audio-analysis/"
+						var id = url.substring(url.lastIndexOf(str)+ str.length);
+						console.log(url, id, startAt);
+						localStorage.setItem(id, startAt);
+						analysis[id] = startAt;
+					});
+				}).catch(function(error) {
+					console.log(error);
+				});
+			} else {
+				analysis[data.audio_features[i].id] = lsid;
+				console.log("already got " + data.audio_features[i].id + " in cache");
+			}
+		}	
+	});
+}
+
+Number.prototype.toMMSS = function () {
+    var sec_num = parseInt(this, 10); // don't forget the second param
+    var minutes = Math.floor(sec_num / 60);
+    var seconds = sec_num - (minutes * 60);
+
+    if (minutes < 10) {minutes = "0"+minutes;}
+    if (seconds < 10) {seconds = "0"+seconds;}
+    return minutes+':'+seconds;
+}
+
+Number.prototype.toSS = function () {
+    var sec_num = parseInt(this, 10); // don't forget the second param
+    var seconds = sec_num;
+    if (seconds < 10) {seconds = "0"+seconds;}
+    return seconds;
+}
+
---
+++ HIITify/spotify.js
@@ -0,0 +1,103 @@
+"use strict"
+var Spotify = {
+	client_id: '',
+	token: '',
+	userCountry: '',
+	userId: '',
+	isPlaying: false,
+	login: function() {
+		window.location.replace('https://accounts.spotify.com/authorize?client_id='+Spotify.client_id+'&redirect_uri=http%3A%2F%2Flocalhost%3A8080&scope=user-read-private%20user-read-playback-state%20playlist-read-private%20playlist-read-collaborative%20user-modify-playback-state&response_type=token&state=123');
+	},
+	req: function(url) {
+		return new Request(url,{
+			headers: new Headers({
+				"Authorization": "Bearer "+Spotify.token
+			})
+		});
+	},
+	put: function(url) {
+		return new Request(url,{
+			method: 'PUT',
+			headers: new Headers({
+				"Authorization": "Bearer "+Spotify.token,
+				"Content-Type": "application/json"
+			})
+		});
+	},
+	putBody: function(url, body) {
+		return new Request(url,{
+			method: 'PUT',
+			headers: new Headers({
+				"Authorization": "Bearer "+Spotify.token,
+			}),
+			body: body
+		});
+	},
+	devices: function() {
+		return fetch(Spotify.req("https://api.spotify.com/v1/me/player/devices"))
+			.then(Spotify.handleErrors)
+			.then((response) => {return response.json();});
+	},
+	me: function () {
+		return fetch(Spotify.req("https://api.spotify.com/v1/me"))
+			.then(Spotify.handleErrors)
+			.then((response) => {return response.json();})
+			.then((data) => {
+				Spotify.userId = data.id;
+				Spotify.userCountry = data.country;
+				return data;
+			});			
+	},
+	getPlayLists: function(offset, limit) {
+		return fetch(Spotify.req("https://api.spotify.com/v1/users/spoteafy/playlists?limit="+limit+"&offset="+offset))
+			.then(Spotify.handleErrors)
+			.then((response) => {return response.json();});
+	},
+	getPlaylistTracks: function(offset,limit,playlist,href) {
+		let url = href!=null?href:playlist+"/tracks?fields=items(track(name,id,is_local,is_playable,duration_ms,album(images),artists(name))),next,offset,limit&limit="+limit+"&offset="+offset+"&market="+Spotify.userCountry;
+		return fetch(Spotify.req(url))
+			.then(Spotify.handleErrors)
+			.then((response) => {return response.json();});
+	},
+	getAudioFeatures: function(ids) {
+		// max 100 ids
+		return fetch(Spotify.req("https://api.spotify.com/v1/audio-features/?ids="+ids))
+			.then(Spotify.handleErrors)
+			.then((response) => {return response.json();});
+	},
+	getAudioAnalysis: function(url) {
+		return fetch(Spotify.req(url))
+			.then(Spotify.handleErrors);
+	},
+	pauseSong: function() {
+		return fetch(Spotify.put("https://api.spotify.com/v1/me/player/pause"))
+		.then(Spotify.handleErrors)
+		.then((data) => {
+			Spotify.isPlaying = false;
+	  		console.log("paused"); 			
+		});
+	},
+	startSong: function(id, startPos) {
+		return fetch(Spotify.putBody("https://api.spotify.com/v1/me/player/play",JSON.stringify({"uris":["spotify:track:"+id]})))
+			.then(Spotify.handleErrors)
+			.then((data) => {			
+				fetch(Spotify.put("https://api.spotify.com/v1/me/player/seek?position_ms="+Math.round(startPos * 1000)))
+	    			.then(handleErrors)
+					.then((data) => {
+						Spotify.isPlaying = true;
+	  					console.log("start playing "+id + " at " + startPos);
+					});  
+			});
+	},
+	handleErrors: function(response) {
+	    if (!response.ok) {
+	    	if (response.status === 401) {
+	    		console.log('no auth');
+	    		Spotify.login();
+	    	} else {
+	        	throw Error(response.statusText);
+	        }
+	    }
+	    return response;
+	}
+}
